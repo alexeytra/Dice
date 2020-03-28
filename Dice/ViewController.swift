13ffbf78e2110776500ca718daeb17cc627fa19d
alexeytra <alekceytr@mail.ru>
2020-03-28 18:24:14 +0800

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Алексей Трапезников on 28.03.2020.
+//  Copyright © 2020 Алексей Трапезников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
